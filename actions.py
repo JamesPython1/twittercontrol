5a5a660dcbe2ec53c6993ba063ef1e529693fead
JamesPython1 <46634565+JamesPython1@users.noreply.github.com>
2019-11-02 18:28:09 +0000

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,8 +1,7 @@
 #what is your favourite single-board computer
 def what_is_your_favourite_sbc():
     print('Raspberry Pi 4!')
-
-
+    
 #keywords & actions
 keywords=['What is your favourite single-board computer?']
 actions=[what_is_your_favourite_sbc]
