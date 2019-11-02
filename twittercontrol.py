02654577162f2ea2b53b1bd69017b16b6197390d
JamesPython1 <46634565+JamesPython1@users.noreply.github.com>
2019-11-02 16:49:05 +0000

    Create twittercontrol.py
    First version!

---
+++ twittercontrol.py
@@ -0,0 +1,40 @@
+#Author: James Grace
+#Date: 2nd November 2019
+#Version: 0.1
+
+import twython
+import time
+from actions import *
+
+############### APPLICATION DETAILS ###############
+twitterHandle=""
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+###################################################
+print('Twitter Control - Created by James Grace')
+print('GitHub https://github.com/JamesPython1/twittercontrol/\n')
+
+class TwitterControl(twython.TwythonStreamer):
+    def on_success(self, data):
+        print('Received tweet from @{0}: {1}'.format(data['user']['screen_name'],data['text']))
+        print('\n')
+        actionsToDo=[]
+        for word in keywords:
+            if word in data['text']:
+                actionsToDo.append(word)
+        print(actionsToDo)
+        for item in actionsToDo:
+            index=keywords.index(item)
+            p=actions[index]()
+
+stream = TwitterControl(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+while True:
+    stream.statuses.filter(track=twitterHandle)
